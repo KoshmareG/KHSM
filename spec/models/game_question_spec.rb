02d22678e10a93aac8f26e38a8263a20c1e25c2c
KoshmareG <koshmareg@yandex.ru>
2023-01-15 20:27:33 +0700

    Add tests for help hash

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -22,21 +22,85 @@ RSpec.describe GameQuestion, type: :model do
     end
   end
 
-  context 'user helpers' do
-    it 'correct audience_help' do
-      expect(game_question.help_hash).not_to include(:audience_help)
+  describe '#text' do
+    it 'returns correct text' do
+      expect(game_question.text).to eq(game_question.question.text)
+    end
+  end
 
-      game_question.add_audience_help
+  describe '#level' do
+    it 'returns correct level' do
+      expect(game_question.level).to eq(game_question.question.level)
+    end
+  end
 
-      expect(game_question.help_hash).to include(:audience_help)
+  describe '#help_hash' do
+    context 'uses fifty fifty' do
+      before do
+        expect(game_question.help_hash).not_to include(:fifty_fifty)
+        game_question.add_fifty_fifty
+      end
 
-      ah = game_question.help_hash[:audience_help]
-      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+      it 'returns help hash with fifty fifty' do
+        expect(game_question.help_hash).to include(:fifty_fifty)
+      end
+
+      it 'returns correct varians count' do
+        expect(game_question.help_hash[:fifty_fifty].size).to eq(2)
+      end
+
+      it 'includes correct answer' do
+        expect(game_question.help_hash[:fifty_fifty]).to include(game_question.correct_answer_key)
+      end
     end
 
-    it 'correct #level & #text' do
-      expect(game_question.text).to eq(game_question.question.text)
-      expect(game_question.level).to eq(game_question.question.level)
+    context 'uses audience help' do
+      before do
+        expect(game_question.help_hash).not_to include(:audience_help)
+        game_question.add_audience_help
+      end
+
+      it 'returns help hash with audience help' do
+        expect(game_question.help_hash).to include(:audience_help)
+      end
+
+      it 'returns all answers varians' do
+        expect(game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+      end
+
+      it 'returns hash' do
+        expect(game_question.help_hash[:audience_help]).to be_an_instance_of(Hash)
+      end
+    end
+
+    context 'uses audience help after fifty fifty' do
+      before do
+        game_question.add_fifty_fifty
+        game_question.add_audience_help
+      end
+
+      it 'returns correct answers count' do
+        expect(game_question.help_hash[:audience_help].keys.size).to eq(2)
+      end
+
+      it 'includes correct answer' do
+        expect(game_question.help_hash[:audience_help].keys).to include(game_question.correct_answer_key)
+      end
+    end
+
+    context 'uses friend call' do
+      before do
+        expect(game_question.help_hash).not_to include(:friend_call)
+        game_question.add_friend_call
+      end
+
+      it 'returns help hash with friend call' do
+        expect(game_question.help_hash).to include(:friend_call)
+      end
+
+      it 'returns string' do
+        expect(game_question.help_hash[:friend_call]).to be_an_instance_of(String)
+      end
     end
   end
 end
